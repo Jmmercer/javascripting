685c2b0fd3d5c95b3edadcccb0b2fc822e1a3b2a
Jeffrey <Jmmercer@ucalgary.ca>
2016-12-31 02:26:31 +0000

    fixed for-loop

--- for-loop.js
+++ for-loop.js
@@ -1,6 +1,6 @@
 var total = 0
 var limit = 10
-for (var i = total; i < limit; total += i) {
-  console.log(i)
+for (var i = total; i < limit; i++) {
+  total += i;
 }
-console.log(i)
+console.log(total)
